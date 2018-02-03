2c12c4c34970b1c672a70a62a21f88eaa9af6f9a
grigory111 <en_semenov@mail.ru>
2018-02-04 01:28:31 +0300

    Add arithm opps

--- Calculate.java
+++ Calculate.java
@@ -2,7 +2,10 @@ public class Calculate
 {
 	public static void main(String[] arg)
 	{
-		System.out.println("Calculate...");
+		Integer first = Integer.valueOf(arg[0]);
+		Integer second = Integer.valueOf(arg[1]);
+		Integer sum = first + second;
+		System.out.println("Sum = " + sum);
 	}
 
 }
